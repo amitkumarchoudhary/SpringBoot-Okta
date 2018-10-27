40b93c760a381f83feb258006da53aaa3dcaaa3a
Amit Kumar Choudhary <amit.choudhary@yash.com>
2018-10-27 14:43:23 +0530

    [R+M] new springsamlokta project

---
+++ src/main/java/info/com/akc/portal/SpringBootSaml/SpringBootSamlApplication.java
@@ -0,0 +1,12 @@
+package info.com.akc.portal.SpringBootSaml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSamlApplication.class, args);
+	}
+}
---
+++ src/test/java/info/com/akc/portal/SpringBootSaml/SpringBootSamlApplicationTests.java
@@ -0,0 +1,16 @@
+package info.com.akc.portal.SpringBootSaml;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSamlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
